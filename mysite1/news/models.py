5ae87e46cb3138879da097a7fc8a18c6bcb91b5b
fi_van <fivan999@yandex.ru>
2022-08-15 23:31:07 +0300

    first_commit

--- mysite1/news/models.py
+++ mysite1/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -14,6 +15,9 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
 
     class Meta:
         verbose_name = 'Новость'
@@ -31,3 +35,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(viewname='category', kwargs={'category_id': self.pk})
--- mysite1/news/views.py
+++ mysite1/news/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from news.models import News, Category
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
 
 def index(request):
     news = News.objects.exclude(is_published=0)
     context = {
         'news': news,
-        'title': 'Новости',
-        'category': 'Новости'
+        'title': 'Новости'
     }
     return render(request, 'news/index.html', context)
 
@@ -22,3 +23,11 @@ def get_category(request, category_id):
         'category': category
     }
     return render(request, 'news/category.html', context)
+
+
+def view_news(request, news_id):
+    news_item = get_object_or_404(News, pk=news_id)
+    context = {
+        'news_item': news_item
+    }
+    return render(request, 'news/view_news.html', context)
