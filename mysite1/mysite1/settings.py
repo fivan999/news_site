71fa94226c2e13e94f6ffc83648d2bee243f5edb
fi_van <fivan999@yandex.ru>
2022-08-16 12:57:46 +0300

    second_commit

--- mysite1/mysite1/settings.py
+++ mysite1/mysite1/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite1/static'),
+]
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_UTL = '/media/'
 # Default primary key field type
