c5ddd72298207231cfa7c63a0de864c4cf655e00
fi_van <fivan999@yandex.ru>
2022-08-21 17:36:04 +0300

    some_genius_moves

--- mysite1/news/models.py
+++ mysite1/news/models.py
@@ -12,7 +12,7 @@ class News(models.Model):
     image = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория',
-                                 related_name='get_news')
+                                 related_name='news')
     views = models.IntegerField(default=0, verbose_name='Просмотры')
 
     def __str__(self):
